b0284c3561cac43714529420fe506306152d30b1
Fulgurance <zohran.londais@gmail.com>
2023-04-17 12:28:12 +0100

    Added Ark and Konsole

---
+++ Ark/21.8.0/21.8.0.cr
@@ -0,0 +1,31 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/opt/kf5",
+                            "-DCMAKE_BUILD_TYPE=Release",
+                            "-DBUILD_TESTING=OFF",
+                            "-Wno-dev",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
