89d77a612997c873fb1c4fe5d15a335b49e8fc16
Fulgurance <zohran.londais@gmail.com>
2023-04-27 13:02:08 +0100

    Patched missing includes dirs

--- Plasma-Integration/5.22.4/5.22.4.cr
+++ Plasma-Integration/5.22.4/5.22.4.cr
@@ -9,8 +9,9 @@ class Target < ISM::Software
         super
 
         runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/opt/kf5",
-                            "-DQt5ThemeSupport_LIBRARY=/opt/qt5/lib",
-                            "-DQt5ThemeSupport_INCLUDE_DIR=/opt/qt5/include",
+                            "-DQt5ThemeSupport_LIBRARY=/opt/qt5/lib/libQt5ThemeSupport.a",
+                            "-DQt5ThemeSupport_INCLUDE_DIR=/opt/qt5/include/QtThemeSupport/5.15.2",
+                            "-DQt5DBus_DIR=/opt/qt5/lib/cmake/Qt5DBus",
                             "-DCMAKE_BUILD_TYPE=Release",
                             "-DBUILD_TESTING=OFF",
                             "-Wno-dev",
