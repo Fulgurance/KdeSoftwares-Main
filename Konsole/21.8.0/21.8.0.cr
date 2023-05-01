f1ce62915e4aff3eb1a7234f1703037078e68a1f
Fulgurance <zohran.londais@gmail.com>
2023-05-01 19:53:16 +0100

    Added missing option

--- Konsole/21.8.0/21.8.0.cr
+++ Konsole/21.8.0/21.8.0.cr
@@ -13,6 +13,7 @@ class Target < ISM::Software
                             "-DBUILD_TESTING=OFF",
                             "-Wno-dev",
                             "-DQt5Xml_DIR=/opt/qt5/lib/cmake/Qt5Xml",
+                            "-DQt5Network_DIR=/opt/qt5/lib/cmake/Qt5Network",
                             "-DKF5XmlGui_DIR=/opt/kf5/lib/cmake/KF5XmlGui",
                             "-DKF5Parts_DIR=/opt/kf5/lib/cmake/KF5Parts",
                             "-DKF5KIO_DIR=/opt/kf5/lib/cmake/KF5KIO",
