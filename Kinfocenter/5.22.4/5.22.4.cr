5e1010c152f9fcd67cea109aac02bb97334851ed
Fulgurance <zohran.londais@gmail.com>
2023-05-02 11:10:27 +0100

    Added missing option

--- Kinfocenter/5.22.4/5.22.4.cr
+++ Kinfocenter/5.22.4/5.22.4.cr
@@ -11,6 +11,7 @@ class Target < ISM::Software
         runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/opt/kf5",
                             "-DCMAKE_BUILD_TYPE=Release",
                             "-DBUILD_TESTING=OFF",
+                            "-DQt5Xml_DIR=/opt/qt5/lib/cmake/Qt5Xml",
                             "-Wno-dev",
                             ".."],
                             buildDirectoryPath)
