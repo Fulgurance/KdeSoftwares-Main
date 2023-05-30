598e59210037bdbe46ff6628e00bc6d91c94c08e
fulgurance <zohran.londais@gmail.com>
2023-05-30 22:12:51 +0100

    Removed obsolete phase

--- Kwallet-Pam/5.22.4/5.22.4.cr
+++ Kwallet-Pam/5.22.4/5.22.4.cr
@@ -1,11 +1,5 @@
 class Target < ISM::Software
 
-    def extract
-        super
-
-        moveFile("#{workDirectoryPath(false)}/kwallet-pam-5.22.4","#{workDirectoryPath(false)}/v5.22.4")
-    end
-
     def prepare
         @buildDirectory = true
         super
