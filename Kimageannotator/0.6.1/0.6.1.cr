b15d219b7d8a986cae0043dfca3c08e3d4d76756
Fulgurance <zohran.londais@gmail.com>
2024-05-14 20:37:59 +0100

    Added new softwares and missing dependency

---
+++ Kimageannotator/0.6.1/0.6.1.cr
@@ -0,0 +1,29 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/usr",
+                            "-DCMAKE_BUILD_TYPE=Release",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
