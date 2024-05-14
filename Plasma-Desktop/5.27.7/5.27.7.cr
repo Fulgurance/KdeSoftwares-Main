3efc137071d138ca3427b0add56a8afd810a06f3
Fulgurance <zohran.londais@gmail.com>
2024-05-14 23:28:17 +0100

    Updated plasma launchers

--- Plasma-Desktop/5.27.7/5.27.7.cr
+++ Plasma-Desktop/5.27.7/5.27.7.cr
@@ -27,6 +27,12 @@ class Target < ISM::Software
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
 
+        fileReplaceText("#{buildDirectoryPath}/usr/share/xsessions/plasma.desktop","Exec=/usr/bin/startplasma-x11","Exec=dbus-launch --exit-with-session startplasma-x11")
+        fileReplaceText("#{buildDirectoryPath}/usr/share/xsessions/plasma.desktop","TryExec=/usr/bin/startplasma-x11","TryExec=dbus-launch --exit-with-session startplasma-x11")
+
+        ffileReplaceText("#{buildDirectoryPath}/usr/share/xsessions/plasmawayland.desktop","Exec=/usr/lib/libexec/plasma-dbus-run-session-if-needed /usr/bin/startplasma-wayland","Exec=dbus-launch --exit-with-session startplasma-wayland")
+        fileReplaceText("#{buildDirectoryPath}/usr/share/xsessions/plasmawayland.desktop","TryExec=/usr/bin/startplasma-wayland","TryExec=dbus-launch --exit-with-session startplasma-wayland")
+
         if option("Linux-Pam")
             makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/pam.d")
 
