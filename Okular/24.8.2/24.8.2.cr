7aad99b83ccacbdb8c6f0744a00a0f448c616bb5
Fulgurance <zohran.londais@gmail.com>
2024-11-28 19:03:15 +0000

    Updated dependencies

--- Okular/24.8.2/24.8.2.cr
+++ Okular/24.8.2/24.8.2.cr
@@ -11,7 +11,7 @@ class Target < ISM::Software
         runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr                                                                                            \
                                     -DCMAKE_BUILD_TYPE=Release                                                                                              \
                                     -DBUILD_TESTING=OFF                                                                                                     \
-                                    -DFORCE_NOT_REQUIRED_DEPENDENCIES=\"LibSpectre;CHM;LibZip;DjVuLibre;EPub;QMobipocket;Discount;Poppler;TIFF;KF5KExiv2\"  \
+                                    -DFORCE_NOT_REQUIRED_DEPENDENCIES=\"LibSpectre;CHM;LibZip;DjVuLibre;EPub;QMobipocket;Discount;Poppler;TIFF;KExiv2Qt6\"  \
                                     -Wno-dev                                                                                                                \
                                     ..",
                         path:       buildDirectoryPath)
