00ed2fd19f96a6420cd3bd825d178432a8b74e2d
Fulgurance <zohran.londais@gmail.com>
2025-12-16 18:11:00 +0000

    Added wayland session

--- Plasma-Workspace/6.5.3/Installer.cr
+++ Plasma-Workspace/6.5.3/Installer.cr
@@ -8,10 +8,11 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr    \
-                                    -DCMAKE_BUILD_TYPE=Release      \
-                                    -DBUILD_TESTING=OFF             \
-                                    -Wno-dev                        \
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr        \
+                                    -DCMAKE_BUILD_TYPE=Release          \
+                                    -DBUILD_TESTING=OFF                 \
+                                    -DINSTALL_SDDM_WAYLAND_SESSION=ON   \
+                                    -Wno-dev                            \
                                     ..",
                         path:       buildDirectoryPath)
     end
